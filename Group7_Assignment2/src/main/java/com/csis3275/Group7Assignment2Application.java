849c64adc7b8c67172aae64e74081a3ece6fce0d
Elson-Ricafrente <ricafrentee@student.douglascollege.ca>
2024-02-20 22:27:07 -0800

    Initial commit

---
+++ Group7_Assignment2/src/main/java/com/csis3275/Group7Assignment2Application.java
@@ -0,0 +1,13 @@
+package com.csis3275;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Group7Assignment2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Group7Assignment2Application.class, args);
+	}
+
+}
---
+++ Group7_Assignment2/src/test/java/com/csis3275/Group7Assignment2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csis3275;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Group7Assignment2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
