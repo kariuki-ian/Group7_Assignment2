d1138749f1a40625bb1f8cad9380693307ff54c3
Elson-Ricafrente <ricafrentee@student.douglascollege.ca>
2024-02-20 23:22:30 -0800

    Copied sample image files

--- Group7_Assignment2/src/main/java/com/csis3275/controller/GroupController.java
+++ Group7_Assignment2/src/main/java/com/csis3275/controller/GroupController.java
@@ -13,13 +13,12 @@ import com.csis3275.model.GroupModel;
 public class GroupController {
 
 	@GetMapping("/")
-	private String index(Model model) {
-		String groupDescription = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Pharetra magna ac placerat vestibulum. Ultrices vitae auctor eu augue ut lectus arcu bibendum. Blandit aliquam etiam erat velit scelerisque in dictum non. Sed arcu non odio euismod lacinia at quis. Malesuada fames ac turpis egestas sed tempus urna et pharetra. Lacus vestibulum sed arcu non. Vitae congue eu consequat ac felis donec et. Sit amet justo donec enim diam vulputate. Quisque sagittis purus sit amet. At auctor urna nunc id cursus metus aliquam.";
+	public String index(Model model) {
 
-		List<String> images = Arrays.asList("Image1", "Image2", "Image3");
+		String groupDescription = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Pharetra magna ac placerat vestibulum. Ultrices vitae auctor eu augue ut lectus arcu bibendum. Blandit aliquam etiam erat velit scelerisque in dictum non. Sed arcu non odio euismod lacinia at quis. Malesuada fames ac turpis egestas sed tempus urna et pharetra. Lacus vestibulum sed arcu non. Vitae congue eu consequat ac felis donec et. Sit amet justo donec enim diam vulputate. Quisque sagittis purus sit amet. At auctor urna nunc id cursus metus aliquam.";
+		List<String> images = Arrays.asList("Image1.png", "Image2.png", "Image3.png");
 		GroupModel groupProfile = new GroupModel(images, groupDescription);
 		model.addAttribute("groupProfile", groupProfile);
 		return "index";
 	}
-
 }
