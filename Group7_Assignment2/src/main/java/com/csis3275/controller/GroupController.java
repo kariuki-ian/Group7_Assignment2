917dcfa8a68b35eeada36707003f091ba37c3571
kariuki-ian <113967581+kariuki-ian@users.noreply.github.com>
2024-02-21 17:15:06 -0800

    Revert "Added Ian's Model Class"

--- Group7_Assignment2/src/main/java/com/csis3275/controller/GroupController.java
+++ Group7_Assignment2/src/main/java/com/csis3275/controller/GroupController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import com.csis3275.model.GroupModel;
-import com.csis3275.model.Model_ika_09;
 
 @Controller
 public class GroupController {
@@ -18,7 +17,7 @@ public class GroupController {
 	public String index(Model model) {
 
 		String groupDescription = "Meet the dynamic trio: Augusta Wood, Ian Kariuki, and Elson Ricafrente, an enterprising team of students from the CSIS3275 Software Engineering course. United by their passion for technology and innovation, they are currently channeling their talents into the Walletguard project. This venture is a personal financial management app designed to revolutionize the way individuals manage and monitor their finances. Augusta brings a keen eye for design and user experience, ensuring the app is not only functional but also intuitive and user-friendly. Ian contributes his expertise in coding and software architecture, turning complex functionalities into seamless experiences. Elson, with his background in IT support and a keen interest in cyber security, ensures the app's reliability and security, safeguarding users' financial data. Together, they embody a blend of creativity, technical skill, and a commitment to developing an app that empowers users to take control of their financial well-being.";
-		List<String> images = Arrays.asList("Augusta", "Ian", "Elson");
+		List<String> images = Arrays.asList("Augusta.png", "Ian.png", "Elson.png");
 		GroupModel groupProfile = new GroupModel(images, groupDescription);
 		model.addAttribute("groupProfile", groupProfile);
 		return "index";
@@ -27,21 +26,7 @@ public class GroupController {
 	
 	  @GetMapping("/bio/{imageName}")
 	  public String imageDetails(@PathVariable String imageName, Model model) {
-		  
-		  switch(imageName){
-		  case "Ian":
-			  Model_ika_09 std = new Model_ika_09();
-			    model.addAttribute("member",std);
-			    break;
-		  case "Pan.png":
-			  Model_ika_09 std1 = new Model_ika_09();
-			    model.addAttribute("member",std1);
-			    break;
-			  
-		  
-		  }
-		
-          
+
 	    return "bio/" + imageName;
 	  }
 	
